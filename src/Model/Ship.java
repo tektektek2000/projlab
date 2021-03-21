0dc31e373defa6b57766c8e68fd9d5a38a2f0d6e
Csábi Dániel Botond <dani.botond.21621@gmail.com>
2021-03-21 22:27:06 +0100

    Worked on Ship class, done with methods.

--- src/Model/Ship.java
+++ src/Model/Ship.java
@@ -1,10 +1,46 @@
 package Model;
 
+import java.util.Scanner;
+
 public abstract class Ship {
-    public abstract void Move(Field f);
+    Asteroid asteroid;
+    public void Move(Field f){
+        asteroid.Remove(this);
+        Asteroid dest = f.MovedTo(this);
+        setAsteroid(dest);
+    }
     public abstract void Die();
-    public abstract void Hide();
-    public abstract void SunStormNow();
+    public void Hide(){
+        System.out.println("You hid successfully.");
+    }
+    public void SunStormNow(){
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Is the asteroid drilled through? [Y/N]");
+
+        if(in.nextLine().equals("y") || in.nextLine().equals("Y")){
+
+            System.out.println("Is the asteroid empty? [Y/N]");
+
+            if(in.nextLine().equals("y") || in.nextLine().equals("Y")){
+                System.out.println("\tHide()");
+                Hide();
+            }
+            else if(in.nextLine().equals("n") || in.nextLine().equals("N")){
+                System.out.println("\tDie()");
+                Die();
+            }
+        }
+        else if(in.nextLine().equals("n") || in.nextLine().equals("N")){
+            System.out.println("\tDie()");
+            Die();
+        }
+    }
     public abstract void AsteroidExploding();
-    public abstract void Drill();
+    public void Drill(){
+        asteroid.GetDrilled();
+    }
+    public void setAsteroid(Asteroid a){
+        asteroid = a;
+    }
 }
