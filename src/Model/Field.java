0dae5ea5cc704b787e63ec26bd08106f21c8e207
Norbert László <laszlo.norbi2000@hotmail.com>
2021-03-21 21:21:42 +0100

    Implemented Field.

--- src/Model/Field.java
+++ src/Model/Field.java
@@ -1,6 +1,13 @@
 package Model;
 
+import java.util.ArrayList;
+
 public abstract class Field {
+    ArrayList<Field> Neighbours;
+
     public abstract Asteroid MovedTo(Ship s);
-    public abstract void RemoveNeighbour(Field f);
+    public void RemoveNeighbour(Field f){
+        Neighbours.remove(f);
+        System.out.println("Removed Neighbour: "  + f);
+    }
 }
