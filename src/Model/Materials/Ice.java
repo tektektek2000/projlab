09491f855003c21340d6f77c0736c23836347463
Teodóra Tóth <tothteo99@gmail.com>
2021-03-21 21:35:37 +0100

    materials

--- src/Model/Materials/Ice.java
+++ src/Model/Materials/Ice.java
@@ -1,6 +1,11 @@
 package Model.Materials;
+import java.lang.String;
 
-public abstract class Ice {
-    protected abstract String GetTypeUnique();
-    public abstract void DrilledThroughSunClose();
+public abstract class Ice extends Material {
+    protected  String GetTypeUnique(){
+        return "Ice";
+    }
+    public void DrilledThroughSunClose(Asteroid asteroid){
+        asteroid.Evaporate();
+    }
 }
--- src/Model/Materials/Material.java
+++ src/Model/Materials/Material.java
@@ -1,7 +1,10 @@
 package Model.Materials;
-
+import java.lang.String;
+import Model.Asteroid;
 public abstract class Material {
-    public abstract boolean isSameType();
+    public boolean isSameType(Material m){
+        return m.GetTypeUnique().equals(this.GetTypeUnique());
+    }
     protected abstract String GetTypeUnique();
-    public abstract void DrilledThroughSunClose();
+    public abstract void DrilledThroughSunClose(Asteroid asteroid);
 }
