9ebfa17f681af775301e938e2d6fabb3ff4fc96a
Norbert László <laszlo.norbi2000@hotmail.com>
2021-04-16 03:13:34 +0200

    Hopefully finished FileController.

--- src/Controllers/FileController.java
+++ src/Controllers/FileController.java
@@ -115,4 +115,166 @@ public class FileController {
                 new FileOutputStream(f, true));
         map.Save(ps);
     }
+
+    public boolean Compare(File a, File b) throws BadFileFormat, FileNotFoundException {
+        boolean success = false;
+        Scanner FScanner = new Scanner(a);
+        ArrayList<Pair<Saveable, ArrayList<StringPair>>> ALinkeables = new ArrayList<>();
+        while (FScanner.hasNextLine()) {
+            String data = FScanner.nextLine();
+            if(data.contains("{")){
+                String ClassType = Trim((data.substring(0,data.indexOf('{'))));
+                Pair<Saveable, ArrayList<StringPair>> current = null;
+                switch (ClassType) {
+                    case "Asteroid":
+                        current = new Pair<Saveable, ArrayList<StringPair>>(new Asteroid(NextLineShouldBeUID(FScanner)), new ArrayList<>());
+                        break;
+                    case "Base":
+                        current = new Pair<Saveable, ArrayList<StringPair>>(new Base(NextLineShouldBeUID(FScanner)), new ArrayList<>());
+                        break;
+                    case "Coal":
+                        current = new Pair<Saveable, ArrayList<StringPair>>(new Coal(NextLineShouldBeUID(FScanner)), new ArrayList<>());
+                        break;
+                    case "Iron":
+                        current = new Pair<Saveable, ArrayList<StringPair>>(new Iron(NextLineShouldBeUID(FScanner)), new ArrayList<>());
+                        break;
+                    case "Ice":
+                        current = new Pair<Saveable, ArrayList<StringPair>>(new Ice(NextLineShouldBeUID(FScanner)), new ArrayList<>());
+                        break;
+                    case "PlayerShip":
+                        PlayerShip ps = new PlayerShip(NextLineShouldBeUID(FScanner));
+                        current = new Pair<Saveable, ArrayList<StringPair>>(ps, new ArrayList<>());
+                        break;
+                    case "RobotShip":
+                        RobotShip rs = new RobotShip(NextLineShouldBeUID(FScanner));
+                        current = new Pair<Saveable, ArrayList<StringPair>>(rs, new ArrayList<>());
+                        break;
+                    case "Sector":
+                        Sector s = new Sector(NextLineShouldBeUID(FScanner));
+                        current = new Pair<Saveable, ArrayList<StringPair>>(s, new ArrayList<>());
+                        break;
+                    case "TeleportGate":
+                        TeleportGate t = new TeleportGate(NextLineShouldBeUID(FScanner));
+                        current = new Pair<Saveable, ArrayList<StringPair>>(t, new ArrayList<>());
+                        break;
+                    case "UFO":
+                        UFO u = new UFO(NextLineShouldBeUID(FScanner));
+                        current = new Pair<Saveable, ArrayList<StringPair>>(u, new ArrayList<>());
+                        break;
+                    default:
+                        throw(new BadFileFormat());
+                }
+                do{
+                    data = Trim(FScanner.nextLine());
+                    if(!data.contains("}")){
+                        current.second.add(new StringPair(data.split(":")));
+                    }
+                }while(!data.contains("}"));
+                ALinkeables.add(current);
+            }
+        }
+        FScanner = new Scanner(b);
+        ArrayList<Pair<Saveable, ArrayList<StringPair>>> BLinkeables = new ArrayList<>();
+        while (FScanner.hasNextLine()) {
+            String data = FScanner.nextLine();
+            if(data.contains("{")){
+                String ClassType = Trim((data.substring(0,data.indexOf('{'))));
+                Pair<Saveable, ArrayList<StringPair>> current = null;
+                switch (ClassType) {
+                    case "Asteroid":
+                        current = new Pair<Saveable, ArrayList<StringPair>>(new Asteroid(NextLineShouldBeUID(FScanner)), new ArrayList<>());
+                        break;
+                    case "Base":
+                        current = new Pair<Saveable, ArrayList<StringPair>>(new Base(NextLineShouldBeUID(FScanner)), new ArrayList<>());
+                        break;
+                    case "Coal":
+                        current = new Pair<Saveable, ArrayList<StringPair>>(new Coal(NextLineShouldBeUID(FScanner)), new ArrayList<>());
+                        break;
+                    case "Iron":
+                        current = new Pair<Saveable, ArrayList<StringPair>>(new Iron(NextLineShouldBeUID(FScanner)), new ArrayList<>());
+                        break;
+                    case "Ice":
+                        current = new Pair<Saveable, ArrayList<StringPair>>(new Ice(NextLineShouldBeUID(FScanner)), new ArrayList<>());
+                        break;
+                    case "PlayerShip":
+                        PlayerShip ps = new PlayerShip(NextLineShouldBeUID(FScanner));
+                        current = new Pair<Saveable, ArrayList<StringPair>>(ps, new ArrayList<>());
+                        break;
+                    case "RobotShip":
+                        RobotShip rs = new RobotShip(NextLineShouldBeUID(FScanner));
+                        current = new Pair<Saveable, ArrayList<StringPair>>(rs, new ArrayList<>());
+                        break;
+                    case "Sector":
+                        Sector s = new Sector(NextLineShouldBeUID(FScanner));
+                        current = new Pair<Saveable, ArrayList<StringPair>>(s, new ArrayList<>());
+                        break;
+                    case "TeleportGate":
+                        TeleportGate t = new TeleportGate(NextLineShouldBeUID(FScanner));
+                        current = new Pair<Saveable, ArrayList<StringPair>>(t, new ArrayList<>());
+                        break;
+                    case "UFO":
+                        UFO u = new UFO(NextLineShouldBeUID(FScanner));
+                        current = new Pair<Saveable, ArrayList<StringPair>>(u, new ArrayList<>());
+                        break;
+                    default:
+                        throw(new BadFileFormat());
+                }
+                do{
+                    data = Trim(FScanner.nextLine());
+                    if(!data.contains("}")){
+                        current.second.add(new StringPair(data.split(":")));
+                    }
+                }while(!data.contains("}"));
+                BLinkeables.add(current);
+            }
+        }
+        assert ALinkeables.size() == BLinkeables.size() : "Object number mismatch: " + ALinkeables.size() + " versus " + BLinkeables.size();
+        for(Pair<Saveable, ArrayList<StringPair>> it1 : ALinkeables){
+            boolean found = false;
+            for(Pair<Saveable, ArrayList<StringPair>> it2 : BLinkeables){
+                if(it1.first.GetUID() == it2.first.GetUID()){
+                    found = true;
+                    assert it1.second.size() == it2.second.size() : "Comparison objects: UID1" + it1.first.GetUID() + " UID2" + it2.first.GetUID() + "| Parameter size mismatch";
+                    for(StringPair ParamIt1 : it1.second){
+                        boolean paramFound = false;
+                        for(StringPair ParamIt2 : it2.second){
+                            if(ParamIt1.first.equals(ParamIt2.first)){
+                                paramFound = true;
+                                assert ParamIt1.second.equals(ParamIt2.second) : "Comparison objects: UID1" + it1.first.GetUID() + " UID2" + it2.first.GetUID() + "| Parameter mismatch: "
+                                        + ParamIt1.first + "  It1:" + ParamIt1.second + " It2:" + ParamIt2.second;
+                            }
+                        }
+                        assert !paramFound : "Parameter not found: Object UID" + it1.first.GetUID() + " Paramtype: " + ParamIt1.first;
+                    }
+                    break;
+                }
+            }
+            assert !found : "Object not found: UID" + it1.first.GetUID();
+        }
+
+        for(Pair<Saveable, ArrayList<StringPair>> it1 : BLinkeables){
+            boolean found = false;
+            for(Pair<Saveable, ArrayList<StringPair>> it2 : ALinkeables){
+                if(it1.first.GetUID() == it2.first.GetUID()){
+                    found = true;
+                    assert it1.second.size() == it2.second.size() : "Comparison objects: UID1" + it1.first.GetUID() + " UID2" + it2.first.GetUID() + "| Parameter size mismatch";
+                    for(StringPair ParamIt1 : it1.second){
+                        boolean paramFound = false;
+                        for(StringPair ParamIt2 : it2.second){
+                            if(ParamIt1.first.equals(ParamIt2.first)){
+                                paramFound = true;
+                                assert ParamIt1.second.equals(ParamIt2.second) : "Comparison objects: UID1" + it1.first.GetUID() + " UID2" + it2.first.GetUID() + "| Parameter mismatch: "
+                                        + ParamIt1.first + "  It1:" + ParamIt1.second + " It2:" + ParamIt2.second;
+                            }
+                        }
+                        assert !paramFound : "Parameter not found: Object UID" + it1.first.GetUID() + " Paramtype: " + ParamIt1.first;
+                    }
+                    break;
+                }
+            }
+            assert !found : "Object not found: UID" + it1.first.GetUID();
+        }
+
+        return success;
+    }
 }
