6b94589734c5d5135246d03a0f5c6142b276bb9d
Norbert László <laszlo.norbi2000@hotmail.com>
2021-05-05 20:38:54 +0200

    JavaFX Base commit

---
+++ src/UI/Layout/Game/GameUIController.java
@@ -0,0 +1,8 @@
+package UI.Layout.Game;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class GameUIController {
+
+}
--- src/UI/Main.java
+++ src/UI/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package UI;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("/UI/Layout/Game/GameLayout.fxml"));
+        primaryStage.setTitle("Projlab");
+        primaryStage.setScene(new Scene(root, 800, 300));
         primaryStage.show();
     }
 
